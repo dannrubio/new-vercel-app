caf70f76f5477da655251f2f9a5744d7597145a9
dannrubio <dann.rubio@gmail.com>
2025-09-08 16:26:32 +0800

    feat: next.js + vercel starter

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,7 @@
+export const runtime = 'edge';
+
+export async function GET() {
+  return new Response(JSON.stringify({ message: 'Hello from the API 👋' }), {
+    headers: { 'content-type': 'application/json' },
+  });
+}
